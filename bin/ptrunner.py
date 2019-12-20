fec6e8fabf9e74284b1b265d07c3dc597e70a1c6
Marty Chang <marty.y.chang@gmail.com>
2019-12-20 12:56:38 -0500

    Stub ptrunner.py with argparse

---
+++ bin/ptrunner.py
@@ -0,0 +1,41 @@
+# This is the script used to collect performance test data from Salesforce
+# by executing a test with various parameters.
+#
+#   ptrunner.py \
+#       -t PtLoopLocalVariableSingleUse \
+#       -n 1000 \
+#       -p number_of_accounts=100 \
+#       -r https://example.my.salesforce.com/
+#
+# The example above runs the `PtLoopLocalVariableSingleUse` performance test
+# 1,000 times, where the number of accounts used in the test is 100. The
+# endpoint for the HTTP GET request is specified by the `-r` argument.
+
+import argparse
+
+def _parse_args():
+    """Parse command line arguments from script execution.
+    """
+
+    _parser = argparse.ArgumentParser(
+        description='Run performance tests against Salesforce.')
+    
+    _parser.add_argument('-t', '--testname', type=str,
+                         required=True,
+                         help='the name of the test to run')
+    
+    _parser.add_argument('-r', '--baseurl', type=str,
+                         required=True,
+                         help=('the base URL on which to build '
+                               'the full endpoint with parameters'))
+
+    _parser.add_argument('-p', '--parameters', type=str,
+                         help='URL parameters to add to further definethe test')
+
+    _parser.add_argument('-n', '--numberofruns', type=int,
+                         default=10,
+                         help='the number of times to run the test')
+
+    return _parser.parse_args()
+
+_args = _parse_args()
